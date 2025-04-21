53540bc61f6375ffa0fac82ae04cb7f234b9301a
alexispb2025 <alexispuentesbaron@gmail.com>
2025-04-20 19:47:53 -0500

    Add files via upload

---
+++ Inventario/InventarioUser.py
@@ -0,0 +1,8 @@
+from InicioINV import Inventario
+
+inventario1 = Inventario (input("Ingrese el producto: "), 
+input("Ingrese el tamaño del producto: "), 
+(input("Ingrese el peso del producto: ")),
+int(input("Ingrese la cantidad de productos a ingresar: ")))
+inventario1.ListaInventario()
+
